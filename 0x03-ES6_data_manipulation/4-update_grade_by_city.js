0a5078d36462dc02b0a6a740d8af5fc75861bab7
dagmawi <dagmawi@eaglelionsystems.com>
2024-10-09 02:58:38 +0300

    task 4 fix

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,28 +5,27 @@
  *   firstName: String,
  *   location: String
  * }[]} students - The list of students.
- * @param {*} city - The city of students.
+ * @param {String} city - The city of students.
  * @param {{
  *   studentId: Number,
  *   grade: Number,
  * }[]} newGrades - The new grades to be given to a student.
- * @returns {{id: Number, firstName: String, location: String}[]}
+ * @returns {{id: Number, firstName: String, location: String, grade: Number|String}[]}
  */
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const defaultGrade = { grade: 'N/A' };
-
-  if (students instanceof Array) {
-    return students
-      .filter((student) => student.location === city)
-      .map((student) => ({
-        id: student.id,
-        firstName: student.firstName,
-        location: student.location,
-        grade: (
-          newGrades.filter((grade) => grade.studentId === student.id).pop() ||
-          defaultGrade
-        ).grade,
-      }));
+  if (!(students instanceof Array)) {
+    return [];
   }
-  return [];
+
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const newGrade = newGrades.find(
+        (grade) => grade.studentId === student.id
+      );
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
 }
